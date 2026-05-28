df33dbd4f481cd8f7d271765623b1ecbec6ef2f1
Qiu Difeng <qiudifeng@qq.com>
2026-05-29 00:48:55 +0800

    fix: skip reminder on reopened PR with no assignee (#135)

--- src/handlers/watch-user-activity.ts
+++ src/handlers/watch-user-activity.ts
@@ -1,3 +1,10 @@
+    // Fix: Don't post reminders on reopened if there is no assignee
+    if (context.eventName === "issues.reopened") {
+      const issue = context.payload.issue as IssueType;
+      if (!issue.assignees || issue.assignees.length === 0) {
+        return { message: logger.debug("Skipping reminder on reopened issue with no assignee.").logMessage.raw };
+      }
+    }
 import { RestEndpointMethodTypes } from "@octokit/rest";
 import { updateCronState } from "../cron/workflow";
 import { removeEntryFromDatabase } from "../helpers/remind-and-remove";
